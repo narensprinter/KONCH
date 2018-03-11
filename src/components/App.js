59503b3a42a87184ad45e7ddfcb54fee28cc7205
narayananbtech <narayananbtech@gmail.com>
2018-03-11 21:32:24 +0530

    file updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Banner from './Banner';
+import Section from './Section';
 
 const App = () => (
    <div className="site-wrapper">
        <Banner />
+       <Section />
    </div>
 )
 
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 
 const Section = () => (
-  <div>
-      Search
-  </div>
+ <section className="section-one">
+   <div className="container">
+      <div className="row">
+         <div className="col-8">
+              <img className="sample-img" src="/images/video.png" />
+         </div>
+         <div className="col-4 section-flex">
+            <div className="section-content">
+               <h3 className="site-title">Transcode for compatibility</h3>
+               <p className="site-desc">Transcode & create all the assets you need in one shot.</p>
+            </div>
+         </div>
+      </div>
+    </div>
+ </section>
 )
 
 export default Section;
