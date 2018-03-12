18562343a69c2579b48d0e669d59d100f3d1245d
narayananbtech <narayananbtech@gmail.com>
2018-03-12 17:29:28 +0530

    file updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
 import Section from './Section';
+import Footer from './Footer';
 
 const App = () => (
    <div className="site-wrapper">
        <Banner />
        <Section />
+       <Footer />
    </div>
 )
 
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,109 +2,104 @@ import React from 'react';
 
 
 const Section = () => (
-  <div>
   <div className="container position-relative">
-  <div className="dotted-line"></div>
-  <section className="section-one">
-      <div className="row">
-         <div className="col-md-8">
-              <img className="sample-img" src="/images/video.png" />
-         </div>
-         <div className="col-md-4 section-flex">
-            <div className="section-content">
-               <h3 className="site-title">Transcode for compatibility</h3>
-               <p className="site-desc">Transcode & create all the assets you need in one shot.</p>
+        <div className="dotted-line"></div>
+        <section className="section-one">
+            <div className="row">
+               <div className="col-md-8">
+                    <img className="sample-img" src="/images/video.png" />
+               </div>
+               <div className="col-md-4 section-flex">
+                  <div className="section-content">
+                     <h3 className="site-title">Transcode for compatibility</h3>
+                     <p className="site-desc">Transcode & create all the assets you need in one shot.</p>
+                  </div>
+               </div>
             </div>
-         </div>
-      </div>
- </section>
- <section className="section-two">
-    <div className="row">
-      <div className="col-md-4 section-flex">
-        <div className="section-content">
-           <div className="dotted-circle"></div>
-           <h3 className="site-title">Thumnails in sequence to preview your content.</h3>
-           <p className="site-desc">Transcode & create all the assets you need in one shot.</p>
-        </div>
-      </div>
-      <div className="col-md-8">
-          <img className="" src="/images/image1.png" />
-      </div>
-    </div>
- </section>
- <section className="section-three">
-     <div className="row">
-        <div className="col-md-8">
-             <img className="sample-img" src="/images/slice2.png" />
-        </div>
-        <div className="col-md-4 section-flex">
-           <div className="section-content">
-              <h3 className="site-title">Time stamped transcriptions and subtitles assets</h3>
-              <p className="site-desc">Transcode & create all the assets you need in one shot.</p>
-              <p className="light-red-text"><span className="red-line"></span>When our a.i parser think it may have got it wrong, we let you know and allow you to edit blabh blah...</p>
-           </div>
-        </div>
-     </div>
-</section>
-<section className="section-two">
-   <div className="row">
-     <div className="col-md-4 section-flex">
-       <div className="section-content">
-          <div className="dotted-circle"></div>
-          <h3 className="site-title">Entities extracted from the transcript & images.</h3>
-          <p className="site-desc">Transcode & create all the assets you need in one shot.</p>
-          <p className="">This is a <span className="pro-box">PRO</span> feature, <br/></p>
-          <p className=""><a href="#" className="green-text font-weight-bold">find out more</a></p>
-       </div>
-     </div>
-     <div className="col-md-8">
-         <img className="" src="/images/bar.png" />
-     </div>
-   </div>
-</section>
-<section className="section-three">
-    <div className="row">
-       <div className="col-md-8">
-            <img className="sample-img" src="/images/slice3.png" />
-       </div>
-       <div className="col-md-4 section-flex">
-          <div className="section-content">
-             <h3 className="site-title">Marvelous Jam for a first century of jelly.</h3>
-             <p className="site-desc">Transcode & create all the assets you need one shot.</p>
+       </section>
+       <section className="section-two">
+          <div className="row">
+            <div className="col-md-4 section-flex">
+              <div className="section-content">
+                 <div className="dotted-circle"></div>
+                 <h3 className="site-title">Thumnails in sequence to preview your content.</h3>
+                 <p className="site-desc">Transcode & create all the assets you need in one shot.</p>
+              </div>
+            </div>
+            <div className="col-md-8">
+                <img className="" src="/images/image1.png" />
+            </div>
           </div>
-       </div>
-    </div>
-</section>
-<section className="section-four">
-    <h2 className="main-head mb-3">Developers, we have webhooks & endpoints for you.</h2>
-    <p>Loren Ipsum er ganske enkelt fyldekst fra print- og typografindustrien.<br/>Lorem Ipsum har vaeret standard fyldtekst siden 1500-tallet, hvor en.</p>
-    <div className="row mt-5">
-       <div className="col-md-6 mb-3">
-          <img className="mb-2" src="/images/icon01.png" width="40" />
-         <h3 className="site-title">Create endpoints</h3>
-         <p className="site-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-       </div>
-       <div className="col-md-6 mb-3">
-          <img className="mb-2" src="/images/icon01.png" width="40" />
-         <h3 className="site-title">Create endpoints</h3>
-         <p className="site-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-       </div>
-       <div className="col-md-6 mb-3">
-          <img className="mb-2" src="/images/icon01.png" width="40" />
-         <h3 className="site-title">Create endpoints</h3>
-         <p className="site-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-       </div>
-       <div className="col-md-6 mb-3">
-          <img className="mb-2" src="/images/icon01.png" width="40" />
-         <h3 className="site-title">Create endpoints</h3>
-         <p className="site-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-       </div>
-     </div>
-</section>
- </div>
-
- <footer className="footer">
- </footer>
+       </section>
+       <section className="section-three">
+           <div className="row">
+              <div className="col-md-8">
+                   <img className="sample-img" src="/images/slice2.png" />
+              </div>
+              <div className="col-md-4 section-flex">
+                 <div className="section-content">
+                    <h3 className="site-title">Time stamped transcriptions and subtitles assets</h3>
+                    <p className="site-desc">Transcode & create all the assets you need in one shot.</p>
+                    <p className="light-red-text"><span className="red-line"></span>When our a.i parser think it may have got it wrong, we let you know and allow you to edit blabh blah...</p>
+                 </div>
+              </div>
+           </div>
+      </section>
+      <section className="section-two">
+         <div className="row">
+           <div className="col-md-4 section-flex">
+             <div className="section-content">
+                <div className="dotted-circle"></div>
+                <h3 className="site-title">Entities extracted from the transcript & images.</h3>
+                <p className="site-desc">Transcode & create all the assets you need in one shot.</p>
+                <p className="">This is a <span className="pro-box">PRO</span> feature, <br/></p>
+                <p className=""><a href="#" className="green-text font-weight-bold">find out more</a></p>
+             </div>
+           </div>
+           <div className="col-md-8">
+               <img className="" src="/images/bar.png" />
+           </div>
+         </div>
+      </section>
+      <section className="section-three">
+          <div className="row">
+             <div className="col-md-8">
+                  <img className="sample-img" src="/images/slice3.png" />
+             </div>
+             <div className="col-md-4 section-flex">
+                <div className="section-content">
+                   <h3 className="site-title">Marvelous Jam for a first century of jelly.</h3>
+                   <p className="site-desc">Transcode & create all the assets you need one shot.</p>
+                </div>
+             </div>
+          </div>
+      </section>
+      <section className="section-four">
+          <h2 className="main-head mb-3">Developers, we have webhooks & endpoints for you.</h2>
+          <p>Loren Ipsum er ganske enkelt fyldekst fra print- og typografindustrien.<br/>Lorem Ipsum har vaeret standard fyldtekst siden 1500-tallet, hvor en.</p>
+          <div className="row mt-5">
+             <div className="col-md-6 mb-3">
+                <img className="mb-2" src="/images/icon01.png" width="40" />
+               <h3 className="site-title">Create endpoints</h3>
+               <p className="site-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+             </div>
+             <div className="col-md-6 mb-3">
+                <img className="mb-2" src="/images/icon01.png" width="40" />
+               <h3 className="site-title">Create endpoints</h3>
+               <p className="site-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+             </div>
+             <div className="col-md-6 mb-3">
+                <img className="mb-2" src="/images/icon01.png" width="40" />
+               <h3 className="site-title">Create endpoints</h3>
+               <p className="site-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+             </div>
+             <div className="col-md-6 mb-3">
+                <img className="mb-2" src="/images/icon01.png" width="40" />
+               <h3 className="site-title">Create endpoints</h3>
+               <p className="site-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+             </div>
+           </div>
+      </section>
  </div>
 )
 
