f27156c38abe8d696f40cce7ed80118b914ca6f2
Udhayakumar <tushar33@gmail.com>
2018-03-12 14:55:01 +0530

    style updated

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Search = () => (
   <div className="site-serach">
        <input placeholder="Add any video URL, or just drag a video file here..." type="text" className="form-control" />
-       <button className="btn btn-primary">Make Magic <img src="/images/main_CTA.png" /></button>
+       <button className="btn btn-primary">Make Magic <img src="/images/main_CTA.png" height="50" /></button>
   </div>
 )
 
